b500f646090fc7cc6428f9e0c0b585a208c06fd4
dhruv918 <83133130+dhruv918@users.noreply.github.com>
2021-11-17 22:31:52 +0530

    Add files via upload

---
+++ movingPosition.js
@@ -0,0 +1,45 @@
+AFRAME.registerComponent('camera-zoom-out', {
+    schema: {
+        moveZ: { type: 'number', default: 10 }
+    },
+    tick: function () {
+        
+        this.data.moveZ = this.data.moveZ + 0.005
+        
+        
+        var pos = this.el.getAttribute('position')
+        pos.z = this.data.moveZ
+        this.el.setAttribute('position', { x: pos.x, y: pos.y, z: pos.z })
+    }
+})
+
+AFRAME.registerComponent('fall-down', {
+    schema: {
+        moveZ: { type: 'number', default: 0 }
+    },
+    tick: function () {
+        window.addEventListener('click', (e) => {
+        this.data.moveY = this.data.moveY - 0.0001
+        
+        })
+        var pos = this.el.getAttribute('position')
+        pos.y = this.data.moveY
+        this.el.setAttribute('position', { x: pos.x, y: pos.y, z: pos.z })
+    }
+})
+
+AFRAME.registerComponent('move', {
+    schema: {
+        moveY: { type: 'number', default: 1 }
+    },
+    tick: function () {
+        
+        this.data.moveY = this.data.moveY + 0.005
+        
+        
+        var pos = this.el.getAttribute('position')
+        pos.y = this.data.moveY
+        this.el.setAttribute('position', { x: pos.x, y: pos.y, z: pos.z })
+    }
+})
+
